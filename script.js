462c0e43c722a6732b7eab3bbe53d13d377f788e
ayush-122 <ayushsingh4045@gmail.com>
2023-11-21 23:50:51 +0530

    deploy

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
+
+//selecting all buttons in html by class using querySelectior
 let startBtn = document.querySelector('.start-btn');
 let stopBtn = document.querySelector('.stop-btn');
 let resetBtn = document.querySelector('.reset-btn');
-
+//selecting all hour , minute and second same by query selector
 let hour =document.querySelector('.hour');
 let minute =document.querySelector('.minute');
 let second =document.querySelector('.second');
 
+//time array will contains the hour ,minute and seconds.
 let time =[0 ,0 ,0];
+//timerId variable will be used while using setInterval and clearInterval
 let timerId= 0;
 
+//event lister on button
 startBtn.addEventListener("click",function(){
+
+    // this if condition we have used , so that if user press start button more than 1times than it should returns and not runs agains.
     if(timerId!=0)
     {
         return;
     }
+    //we are adding the disable class so that user can see the start button is now disable.
     startBtn.classList.add("disable");
+
+    //logic for time increment 
     timerId=setInterval(()=>{
         time[2]++
         if(time[2]==60)
@@ -27,10 +37,12 @@ startBtn.addEventListener("click",function(){
             time[0]++;
             time[1]=0;
         }
+        //displaying time
         displayTime();
     },1000)
 })
-
+ //stop button should stop the stopwatch after clicking so we are clearing the timeInterval and
+ // also setting timerid equal to 0 so that start button behaves properly
 stopBtn.addEventListener("click", function(){
     stopBtn.classList.add("disable");
     startBtn.classList.remove("disable");
@@ -38,7 +50,7 @@ stopBtn.addEventListener("click", function(){
     timerId=0;
     
 })
-
+//reset button will reset the stop watch.
 resetBtn.addEventListener("click",function(){
     startBtn.classList.remove("disable");
     stopBtn.classList.remove("disable");
@@ -49,7 +61,8 @@ resetBtn.addEventListener("click",function(){
     
 })
 
-
+//it will display time, we have used padStart to append 0 to make it of length 2 and padEnd to  
+//append : and make it of 4 length
 function displayTime()
 {
     hour.innerText=time[0].toString().padStart(2,"0").padEnd(4," :");
